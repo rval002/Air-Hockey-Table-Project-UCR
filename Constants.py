0dbdf94f244deaa699fa857ac07a180fe96f0c38
rvalv002 <rvalv002@ucr.edu>
2021-01-11 01:12:30 -0800

    Updated constants

--- Constants.py
+++ Constants.py
@@ -1,1 +1,38 @@
+# **************************************************************************** #
+# Air Hockey Table Project
 
+# Author: Ricardo Valverde
+# -------------------------
+
+#This Code is Created for UCR's EE175 Senior Design Project
+
+# This Code contains The Constants used in the Project Such as dimensions.
+
+# **************************************************************************** #
+
+
+#anything that is zero must still be measured. Dimensional units ???
+#Please add any global constants Here.
+
+
+# ------------- CAMERA -------------
+FRAMERATE = 0 # Might be put in with the camera section.
+
+
+
+# ------------- DIMENSIONS -------------
+#  dimensions units
+FACE_WIDTH = 0
+FACE_LENGTH = 0
+GOAL_CENTER = 0
+GOAL_SIZE = 0
+
+# Objects sizes units
+PUCK_RADIUS = 0
+PADDLE_RADIUS = 0
+
+# -------------- MOTORS --------------
+#  limitations
+MAX_ACCELERATION = 0
+MAX_DECELERATION = 0
+MAX_SPEED = 0
